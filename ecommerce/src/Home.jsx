e6230913bc71f3d0b40b7a7957c49af74bf317ba
Muhammadcodes112 <funuyallen@gmail.com>
2024-08-21 09:34:53 -0700

    again

--- ecommerce/src/Home.jsx
+++ ecommerce/src/Home.jsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom'
 import { FaArrowRightLong } from "react-icons/fa6";
 import galaxyFold6 from './assets/galaxy fold 6.png';
 import galaxyFold5 from './assets/galaxy fold 6.png';
+import { FiTruck } from 'react-icons/fi';
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { HiOutlineReceiptPercent } from 'react-icons/hi';
+import { BiHeadphone } from 'react-icons/bi';
 import './Home.css';
 
 
@@ -56,6 +60,45 @@ const Home = () => {
                 </div>
             </div>
          </div>
+      </div>
+      <div className="about">
+            <div className="container">
+                <div className="box">
+                    <div className="icon">
+                        <FiTruck />
+                    </div>
+                    <div className="detail">
+                        <h3>Free SHipping</h3>
+                        <p>Order above $1000</p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="icon">
+                        <BsCurrencyDollar />
+                    </div>
+                    <div className="detail">
+                        <h3>Return and Refund</h3>
+                        <p>Money Back Guara ntee</p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="icon">
+                        <HiOutlineReceiptPercent />
+                    </div>
+                    <div className="detail">
+                        <h3>Member Discount</h3>
+                        <p>On Every Order</p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="icon">
+                        <BiHeadphone />
+                    </div>
+                    <div className="detail">
+                        <h3>Free SHipping</h3>
+                    </div>
+                </div>
+            </div>
       </div>
     </div>
   )
