57775ad6ff89877fca07d609206c34dc745ee609
Muhammadcodes112 <funuyallen@gmail.com>
2024-08-22 10:40:16 -0700

    updated home page

--- ecommerce/src/Home.jsx
+++ ecommerce/src/Home.jsx
@@ -4,14 +4,18 @@ import { FaArrowRightLong } from "react-icons/fa6";
 import galaxyFold6 from './assets/galaxy fold 6.png';
 import galaxyFold5 from './assets/galaxy fold 6.png';
 import { FiTruck } from 'react-icons/fi';
-import { BsCurrencyDollar } from 'react-icons/bs';
-import { HiOutlineReceiptPercent } from 'react-icons/hi';
+import { BsCurrencyDollar, BsEye } from 'react-icons/bs';
+import { CiPercent } from 'react-icons/ci';
 import { BiHeadphone } from 'react-icons/bi';
+// import { BsEye } from 'react-icons/bs';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import Homeproduct from './Homeproduct';
 import './Home.css';
 
 
-
 const Home = () => {
+    
   return (
     <div>
       <div className='top_banner'>
@@ -83,7 +87,7 @@ const Home = () => {
                 </div>
                 <div className="box">
                     <div className="icon">
-                        <HiOutlineReceiptPercent />
+                        <CiPercent />
                     </div>
                     <div className="detail">
                         <h3>Member Discount</h3>
@@ -95,10 +99,38 @@ const Home = () => {
                         <BiHeadphone />
                     </div>
                     <div className="detail">
-                        <h3>Free SHipping</h3>
+                        <h3>Customer Support</h3>
+                        <p>Every Time Call Support</p>
                     </div>
                 </div>
             </div>
+      </div>
+      <div className="product">
+        <h2>Top Products</h2>
+        <div className="container">
+            {
+                Homeproduct.map((curElm) =>
+                {
+                    return(
+                        <div className="box" key={curElm.id}>
+                            <div className="img_box">
+                                <img className='images' src={curElm.Img} alt={curElm.Title} />
+                                <div className="icon">
+                                    <li><AiOutlineShoppingCart /></li>
+                                    <li><BsEye /></li>
+                                    <li><AiOutlineHeart /></li>
+                                </div>
+                            </div>
+                            <div className="detail">
+                                <p>{curElm.Cat}</p>
+                                <h3>{curElm.Title}</h3>
+                                <h4>{curElm.Price}</h4>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
       </div>
     </div>
   )
